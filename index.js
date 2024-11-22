b7e30b17cf62f294e4387e27eaf7747d9a6a99ee
imtiyaz9876 <md.imtiyazalam9876@gmail.com>
2024-11-23 00:24:30 +0530

    githubaction learning

---
+++ index.js
@@ -0,0 +1,14 @@
+const express=require("express");
+
+    const app = express();
+    
+    app.get("/",async(req,res)=>{
+        res.send({
+            message:"api is working fine"
+        })
+    })
+    const PORT=3000
+    
+    app.listen(PORT,()=>{
+        console.log(`server is listing on port ${PORT}`)
+    })
